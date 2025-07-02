a753619b7980b33417de78733b41d0f62b268a9f
janusjansen <janusjansen@users.noreply.github.com>
2025-07-02 10:04:49 +0000

    formatting

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ const protocol = new Protocol();
 maplibregl.addProtocol("pmtiles", protocol.tile);
 
 const map = new maplibregl.Map({
-    container: 'mijnkaart', 
+    container: 'mijnkaart',
     style: './assets/style.json',
-    center: [5.66509, 51.96857], 
+    center: [5.66509, 51.96857],
     zoom: 13 // starting zoom
 });
 
@@ -17,50 +17,50 @@ const map = new maplibregl.Map({
 // });
 
 map.on('load', () => {
-        map.addSource('route', {
-            'type': 'geojson',
-             data: './assets/wandeling.geojson'
-            }
-        );
-        map.addLayer({
-  'id': 'route-halo',
-  'type': 'line',
-  'source': 'route',
-  'layout': {
-    'line-join': 'round',
-    'line-cap': 'round'
-  },
-  'paint': {
-    'line-color': '#ffffff',
-    'line-width': [
-      'interpolate',
-      ['linear'],
-      ['zoom'],
-      10, 5,
-      15, 12
-    ]
-  }
-});
-
-map.addLayer({
-  'id': 'route',
-  'type': 'line',
-  'source': 'route',
-  'layout': {
-    'line-join': 'round',
-    'line-cap': 'round'
-  },
-  'paint': {
-    'line-color': '#0066cc',
-    'line-opacity': 0.85,
-    'line-width': [
-      'interpolate',
-      ['linear'],
-      ['zoom'],
-      10, 3,
-      15, 8
-    ]
-  }
-});
-
+    map.addSource('route', {
+        'type': 'geojson',
+        data: './assets/wandeling.geojson'
+    }
+    );
+    map.addLayer({
+        'id': 'route-halo',
+        'type': 'line',
+        'source': 'route',
+        'layout': {
+            'line-join': 'round',
+            'line-cap': 'round'
+        },
+        'paint': {
+            'line-color': '#ffffff',
+            'line-width': [
+                'interpolate',
+                ['linear'],
+                ['zoom'],
+                10, 5,
+                15, 12
+            ]
+        }
     });
+
+    map.addLayer({
+        'id': 'route',
+        'type': 'line',
+        'source': 'route',
+        'layout': {
+            'line-join': 'round',
+            'line-cap': 'round'
+        },
+        'paint': {
+            'line-color': '#0066cc',
+            'line-opacity': 0.85,
+            'line-width': [
+                'interpolate',
+                ['linear'],
+                ['zoom'],
+                10, 3,
+                15, 8
+            ]
+        }
+    });
+
+});
